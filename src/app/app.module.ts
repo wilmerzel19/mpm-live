1cc81b1f2d104d03142fa7caa727fc18c4c6c5ec
wilmer <est.wzelayam@uml.edu.ni>
2023-03-10 10:56:19 -0600

    app de angular live

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterLink } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { IdiomasComponent } from './components/idiomas/idiomas.component';
+import { RouterModule } from '@angular/router';
+import { HistorialComponent } from './components/historial/historial.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    EventosComponent,
+    IdiomasComponent,
+    HistorialComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot ([
+      {path: 'eventos', component:EventosComponent},
+      {path: 'home', component:HomeComponent},
+      {path: 'Historial', component:HistorialComponent},
+      {path: 'menu', component:MenuComponent},
+
+
+
+
+
+    ]),
+    BrowserAnimationsModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
